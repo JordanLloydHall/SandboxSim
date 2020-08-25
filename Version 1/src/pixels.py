be80c98253c78b2fa490c9b7be547522f0a74d54
PlatinumNinja72 <Jordanhall123@googlemail.com>
2020-08-25 21:39:35 +0100

    Fixed the branch merge issues

--- Version 1/src/pixels.py
+++ Version 1/src/pixels.py
@@ -2,25 +2,24 @@ import pygame
 
 # ---- Pixel Objects
 class Pixel:
-	def __init__(self, posX, posY):
-		self.posX = posX
-		self.posY = posY
-		self.Color = (25,25,25)
+    def __init__(self, posX, posY):
+        self.posX = posX
+        self.posY = posY
+        self.Color = (25,25,25)
 
-	def draw_pixel(self, screen):
-		print(self.Color)
-		pygame.draw.rect(screen, self.Color, (self.posX - 50 * 0.75, self.posY -50 * 0.75, 25, 25))
+    def draw_pixel(self, screen):
+        pygame.draw.rect(screen, self.Color, (self.posX - 50 * 0.75, self.posY -50 * 0.75, 25, 25))
 
 
 class Sand(Pixel):
-	def __init__(self, posX, posY):
-		
-		Pixel.__init__(self, posX, posY)
-		self.Color = (0,255,255)
+    def __init__(self, posX, posY):
+        
+        Pixel.__init__(self, posX, posY)
+        self.Color = (0,255,255)
 
 
 class Pixel_Cursor(Pixel):
-	def __init__(self, posX, posY):
-		
-		Pixel.__init__(self, posX, posY)
-		self.Color = (100,0,100)
+    def __init__(self, posX, posY):
+        
+        Pixel.__init__(self, posX, posY)
+        self.Color = (100,0,100)
