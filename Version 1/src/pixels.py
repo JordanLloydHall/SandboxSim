2e31ed2249159c3af189ff719b32568187715540
PlatinumNinja72 <Jordanhall123@googlemail.com>
2020-08-26 22:35:22 +0100

    Added more pixel behaviour

--- Version 1/src/pixels.py
+++ Version 1/src/pixels.py
@@ -6,6 +6,8 @@ class Pixel:
         self.pos_x = pos_x
         self.pos_y = pos_y
 
+        self.buoyancy = 0
+
     def draw_pixel(self, screen):
         pygame.draw.rect(screen, self.color, (self.pos_x * 50 + 50 * 0.25, self.pos_y * 50 +50 * 0.25, 25, 25))
 
@@ -16,7 +18,7 @@ class Grey(Pixel):
         self.pos_y = pos_y
         self.color = (25,25,25)
 
-    def update(self, worldGrid):
+    def update(self, world_grid):
         return
 
     def getType(self):
@@ -29,11 +31,19 @@ class Sand(Pixel):
         Pixel.__init__(self, pos_x, pos_y)
         self.color = (0,255,255)
 
-    def update(self, worldGrid):
+        self.buoyancy = 0.5
 
+    def update(self, world_grid):
+
+        if world_grid.get_current_pixel(self.pos_x, self.pos_y) != self:
+            for x in range(-1,2):
+                if world_grid.get_next_pixel(self.pos_x+x, self.pos_y+1) == None:
+                    world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y+y))
+                    return
+        
         for x in range(-1,2):
-            if worldGrid.getPixel(self.pos_x+x, self.pos_y-1) in ["AIR", "WATER"]:
-                worldGrid.swapPixels((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y-1))
+            if world_grid.get_current_pixel(self.pos_x+x, self.pos_y-1).buoyancy < self.buoyancy:
+                world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y-1))
                 return
 
 
@@ -46,12 +56,20 @@ class Water(Pixel):
         Pixel.__init__(self, pos_x, pos_y)
         self.color = (156,211,219)
 
-    def update(self, worldGrid):
-        for y in range(-1,1):
+        self.buoyancy = 1
+
+    def update(self, world_grid):
+        if world_grid.get_current_pixel(self.pos_x, self.pos_y) != self:
             for x in range(-1,2):
-                if worldGrid.getPixel(self.pos_x+x, self.pos_y+y) in ["AIR"]:
-                    worldGrid.swapPixels((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y+y))
+                if world_grid.get_next_pixel(self.pos_x+x, self.pos_y+1) == None:
+                    world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y+y))
                     return
+
+        for x in range(-1,2):
+            
+            if world_grid.get_current_pixel(self.pos_x+x, self.pos_y-1).buoyancy < self.buoyancy:
+                world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y-1))
+                return
 
     def getType(self):
         return "WATER"
