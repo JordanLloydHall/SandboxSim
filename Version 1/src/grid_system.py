d53d2e99ee9eec5b321f24c3e60ba1091596cf5f
PlatinumNinja72 <Jordanhall123@googlemail.com>
2020-08-27 20:37:00 +0100

    Added functions to allow pixels to interact with World_Grid

--- Version 1/src/grid_system.py
+++ Version 1/src/grid_system.py
@@ -1,12 +1,16 @@
 import numpy as np
+from pixels import *
 
 class World_Grid:
 
-    def __init__(self, width, height, pxwidth, screen):
+    def __init__(self, width, height, pxwidth, screen, current_pixel_grid):
         self.width = width
         self.height = height
         self.pxwidth = pxwidth
         self.screen = screen
+
+        self.current_pixel_grid = current_pixel_grid
+        self.next_pixel_grid = Grid_Layer(current_pixel_grid.n_hor, current_pixel_grid.n_ver)
 
     def draw_grid(self, pixel_grid):
         for pixel_row in pixel_grid:
@@ -32,6 +36,45 @@ class World_Grid:
     def set_pxwidth(self, pxwidth):
         self.pxwidth = pxwidth
 
+    def step_pixels(self):
+        self.next_pixel_grid = Grid_Layer(self.current_pixel_grid.n_hor, self.current_pixel_grid.n_ver)
+
+        for y in range(self.current_pixel_grid.n_ver):
+            for x in range(self.current_pixel_grid.n_hor):
+                pixel = self.current_pixel_grid.grid[y,x]
+                if pixel != None:
+                    pixel.has_stepped = False
+                    self.next_pixel_grid.grid[y,x] = pixel
+
+        for row in self.current_pixel_grid.grid:
+            for pixel in row:
+                if pixel != None and pixel.has_stepped == False:
+                    pixel.update(self)
+                    pixel.has_stepped = True
+
+        self.current_pixel_grid = self.next_pixel_grid
+
+    def get_current_pixel(self, x, y):
+        return self.current_pixel_grid.grid[y,x] if self.current_pixel_grid.grid[y,x] else Pixel(x,y)
+
+    def get_next_pixel(self, x, y):
+        return self.next_pixel_grid.grid[y,x] if self.next_pixel_grid.grid[y,x] else Pixel(x,y)
+
+    def move_pixel(self, old_pos, new_pos):
+        if 0 <= old_pos[0] < self.width and 0 <= old_pos[1] < self.height and 0 <= new_pos[0] < self.width and 0 <= new_pos[1] < self.height:
+
+            self.next_pixel_grid.grid[old_pos[1], old_pos[0]] = self.current_pixel_grid.grid[new_pos[1], new_pos[0]]
+            if self.next_pixel_grid.grid[old_pos[1], old_pos[0]] != None:
+                self.current_pixel_grid.grid[new_pos[1], new_pos[0]].pos_x = old_pos[0]
+                self.current_pixel_grid.grid[new_pos[1], new_pos[0]].pos_y = old_pos[1]
+                self.current_pixel_grid.grid[new_pos[1], new_pos[0]].has_stepped = True
+
+            self.next_pixel_grid.grid[new_pos[1], new_pos[0]] = self.current_pixel_grid.grid[old_pos[1], old_pos[0]]
+            self.current_pixel_grid.grid[old_pos[1], old_pos[0]].pos_x = new_pos[0]
+            self.current_pixel_grid.grid[old_pos[1], old_pos[0]].pos_y = new_pos[1]
+
+
+
 class Grid_Layer:
     
     def __init__(self, n_hor, n_ver):
@@ -43,4 +86,3 @@ class Grid_Layer:
         for x in range(0, 10):
             for y in range(0, 10):
                 self.grid[y][x] = obj(x, y)
-    
--- Version 1/src/pixels.py
+++ Version 1/src/pixels.py
@@ -6,10 +6,15 @@ class Pixel:
         self.pos_x = pos_x
         self.pos_y = pos_y
 
-        self.buoyancy = 0
+        self.buoyancy = 1
+
+        self.has_stepped = False
 
     def draw_pixel(self, screen):
         pygame.draw.rect(screen, self.color, (self.pos_x * 50 + 50 * 0.25, self.pos_y * 50 +50 * 0.25, 25, 25))
+
+    def getType(self):
+        return "DEFAULT"
 
 class Grey(Pixel):
 
@@ -29,22 +34,17 @@ class Sand(Pixel):
     def __init__(self, pos_x, pos_y):
         
         Pixel.__init__(self, pos_x, pos_y)
-        self.color = (0,255,255)
+        self.color = (239, 221, 111)
 
-        self.buoyancy = 0.5
+        self.buoyancy = 0
 
     def update(self, world_grid):
 
-        if world_grid.get_current_pixel(self.pos_x, self.pos_y) != self:
-            for x in range(-1,2):
-                if world_grid.get_next_pixel(self.pos_x+x, self.pos_y+1) == None:
-                    world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y+y))
-                    return
-        
-        for x in range(-1,2):
-            if world_grid.get_current_pixel(self.pos_x+x, self.pos_y-1).buoyancy < self.buoyancy:
+        for x in [0,-1,1]:
+            if world_grid.get_current_pixel(self.pos_x+x, self.pos_y-1).buoyancy > self.buoyancy:
                 world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y-1))
                 return
+
 
 
     def getType(self):
@@ -56,18 +56,12 @@ class Water(Pixel):
         Pixel.__init__(self, pos_x, pos_y)
         self.color = (156,211,219)
 
-        self.buoyancy = 1
+        self.buoyancy = 0.5
 
     def update(self, world_grid):
-        if world_grid.get_current_pixel(self.pos_x, self.pos_y) != self:
-            for x in range(-1,2):
-                if world_grid.get_next_pixel(self.pos_x+x, self.pos_y+1) == None:
-                    world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y+y))
-                    return
 
-        for x in range(-1,2):
-            
-            if world_grid.get_current_pixel(self.pos_x+x, self.pos_y-1).buoyancy < self.buoyancy:
+        for x in [0,-1,1]:
+            if world_grid.get_current_pixel(self.pos_x+x, self.pos_y-1).buoyancy > self.buoyancy:
                 world_grid.move_pixel((self.pos_x,self.pos_y), (self.pos_x+x, self.pos_y-1))
                 return
 
